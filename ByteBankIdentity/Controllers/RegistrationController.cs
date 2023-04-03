3043ad8cf42a9e26cce18387c2c02ecc1bfeffdf
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-04-03 09:05:01 -0300

    Remove comments

--- ByteBankIdentity/Controllers/RegistrationController.cs
+++ ByteBankIdentity/Controllers/RegistrationController.cs
@@ -100,8 +100,6 @@ namespace ByteBankIdentity.Controllers
   }
 
   // POST: Registration/Login
-  // To protect from overposting attacks, enable the specific properties you want to bind to.
-  // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
   [HttpPost]
   [ValidateAntiForgeryToken]
   public async Task<IActionResult> Login(User userModel)
